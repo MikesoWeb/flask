ee1b3eb83d1f2668f06bee368feff2e1829784b4
MikesoWeb <klionpsyx@gmail.com>
2022-09-27 10:50:17 +0300

    add_readme

---
+++ Readme.md
@@ -0,0 +1,8 @@
+﻿# Escape
+# Convert the characters &, <, >, ‘, and ” in string s to HTML-safe sequences.
+# Use this if you need to display text that might contain such characters in HTML.
+# Marks return value as markup string.
+
+# Inline CSS
+
+# request.args.get
--- hello.py
+++ hello.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    # http://127.0.0.1:5000/?name=Tom
+    name = request.args.get("name")
+    style_name = '<p style="color:red; text-align:center";>{}</p>!'.format(name)
+    return 'Hello, ' + escape(style_name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

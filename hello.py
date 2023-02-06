80499dcd68e8fe1e976846cecf7c1dcc3775695d
Михаил Терехов <47977649+MikesoWeb@users.noreply.github.com>
2023-02-06 08:56:53 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,14 +2,11 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    # http://127.0.0.1:5000/?name=Tom
+@app.route("/")
+def hello() -> str:
     name = request.args.get("name")
-    style_name = '<p style="color:red; text-align:center";>{}</p>!'.format(name)
-    return 'Hello, ' + escape(style_name)
-
+    style_name = f'<p style="color:red; text-align:center">{name}</p>!'
+    return f'Hello, {escape(style_name)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
